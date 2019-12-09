ed6c9c841bbf34e3b2076d6c22286605ad50aba0
Damien <mail@damientimewell.com>
2019-12-09 14:02:21 +0100

    Removed redux things

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { cart } from 'reducers/cart'
 
 export const Product = ({ product }) => {
-  const dispatch = useDispatch()
-
   return (
     <article className="product">
       <span className="emoji" role="img" aria-label={product.title}>{product.emoji}</span>
@@ -13,7 +9,7 @@ export const Product = ({ product }) => {
       <button
         type="button"
         disabled={product.inventory === 0}
-        onClick={() => dispatch(cart.actions.addItem(product))}>
+        onClick={() => { }}>
         Add to cart
       </button>
     </article>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Product } from './Product'
 
 export const Products = () => {
-  const allProducts = useSelector((state) => state.products)
+  // const allProducts = useSelector((state) => state.products)
+
+  // TODO - fetch all products from the store
+  const allProducts = []
 
   return (
     <div className="products">
